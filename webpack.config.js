0128ac38f0ff28b6a757058113b3b5188803e326
maryzam <zamo.code@gmail.com>
2018-05-12 23:42:02 +0300

    Setup the project with npm, webpack, babel

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+ var path = require('path');
+ var webpack = require('webpack');
+
+ module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        inline: true,
+        contentBase: './dist',
+        port: 3000
+    },
+     module: {
+         rules: [
+             {
+                 test: /\.jsx?$/,                 
+                 exclude: /(node_modules)/,
+                 use: 'babel-loader'
+             },
+             {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+             }
+         ]
+     }
+ };

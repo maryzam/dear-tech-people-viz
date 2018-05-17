d5a9b8df2261409aadef49f1b35cdda58ad3118c
maryzam <zamo.code@gmail.com>
2018-05-18 01:02:41 +0300

    Dome with main features of the OverallBySector Component

--- src/components/OverallBySector/Visualization.js
+++ src/components/OverallBySector/Visualization.js
@@ -8,6 +8,9 @@ import Provider from "../../utils/dataProvider";
 const sectors = Provider.getSectors();
 const data = Provider.getOverallBySectors();
 
+const labelOffset = 70;
+const padding = { x: 10, y: 10 }
+
 class OverallBySectorStats extends React.PureComponent {
 
 	constructor(props) {
@@ -15,71 +18,129 @@ class OverallBySectorStats extends React.PureComponent {
 
 		this.setVizRef = (element) => { this.viz = element; };
 
-		const { race, role, height, width } = this.props;
-
-		this.scaleSectors = d3.scaleBand()
-								.domain(sectors)
-								.range([0, height])
-								.round(true);
-		
-		this.scaleFreq = d3.scaleLinear().range([0, width]);
-
-		const maxRadius = this.scaleSectors.bandwidth() / 2;
-		const maxCount = d3.max(data[race], (d) => (Math.max(d[role].male, d[role].female)));
-		const minCount = d3.min(data[race], (d) => (Math.min(d[role].male, d[role].female)));
-		this.scaleTotal = d3.scaleLinear().range([3, maxRadius]).domain([minCount, maxCount]);
+		this.scaleSectors = d3.scaleBand().domain(sectors).round(true);
+		this.scaleFreq = d3.scaleLinear();
+		this.scaleTotal = d3.scaleLinear();
 	}
 
 	componentDidMount() {
-		// todo
+		
+		this.node = d3.select(this.viz);
+		this.updateViz();
 	}
 
 	componentDidUpdate() {
-		// todo
+		this.updateViz();
 	}
 
 	updateViz() {
 
+		this.updateScales();
+
+		const { race, role, animDuration } = this.props;
+
+		const nodes = this.node
+			.selectAll(".sector")
+			.data(data[race], function(d) { 
+				return d ? d.sector : this.dataset.sector; 
+			});
+
+		nodes
+			.transition().duration(animDuration)
+			.attr("transform", (d) => (`translate(0, ${this.scaleSectors(d.sector)})`));
+
+		nodes
+			.select(".male")
+			.transition().duration(animDuration)
+			.attr("cx", (d) => this.scaleFreq(d[role].freq.male))
+			.attr("r", (d) => this.scaleTotal(d[role].male))
+			.style("fill-opacity", (d) => (d[role].male > 0 ? 0.5 : 1e-6));
+
+		nodes
+			.select(".female")
+			.transition().duration(animDuration)
+			.attr("cx", (d) => this.scaleFreq(d[role].freq.female))
+			.attr("r", (d) => this.scaleTotal(d[role].female))
+			.style("fill-opacity", (d) => (d[role].female > 0 ? 0.5 : 1e-6));
+
+		nodes
+			.select(".axis")
+			.transition().duration(animDuration)
+			.attr("x1", this.scaleFreq(0))
+			.attr("x2", this.scaleFreq(1));
+
+		nodes
+			.select(".gap")
+			.transition().duration(animDuration)
+			.attr("x1", (d) => this.scaleFreq(d[role].freq.female))
+			.attr("x2", (d) => this.scaleFreq(d[role].freq.male))
+			.style("stroke", (d) => (d[role].freq.female < 0.5 ? "steelblue": "tomato"));
+
+		nodes
+			.select(".label")
+			.transition().duration(animDuration)
+			.style("fill-opacity", (d) => (d[role].all > 0 ? 1 : 0.3 ))
+
 	}
 
 	updateScales() {
+		const { race, role, height, width } = this.props;
 
+		const vizHeight = height - padding.y * 2;
+		const vizWidth = width - labelOffset - padding.y * 2;
+
+		this.scaleSectors.range([0, vizHeight]);
+		this.scaleFreq.range([0, vizWidth]);
+
+		const maxRadius = this.scaleSectors.bandwidth() / 2;
+		const maxCount = d3.max(data[race], (d) => (Math.max(d[role].male, d[role].female)));
+		this.scaleTotal.range([3, maxRadius]).domain([0, maxCount]);
 	}
 
 	render() {
 
 		const { width, height, race, role } = this.props;
-		const xOffset = width / 2;
-		const margin = 50; //todo
+		const xMiddle = width / 2;
+		const xMin = this.scaleFreq(0);
+		const xMax = this.scaleFreq(1);
+
 		return (
 			<svg ref={ this.setVizRef }
 					className="overall-by-gender"
 					width={width} 
-					height={height - margin} >
+					height={height} >
+				<g transform={`translate(${labelOffset},${padding.y})`}>
 				{
 					data[race].map((d) => {
 						return (
 							<g key={ d.sector }
+								data-sector={ d.sector }
+								className="sector"
 								transform={`translate(0, ${this.scaleSectors(d.sector)})`}>
-							<circle 
-								cx={this.scaleFreq(d[role].freq.male)} cy={0}
-								r={ this.scaleTotal(d[role].male)} 
-								style={{ fill: "steelblue" }}/>	
-							<circle 
-								cx={this.scaleFreq(d[role].freq.female)} cy={0} 
-								r={ this.scaleTotal(d[role].female)} 
-								style={{ fill: "tomato" }} />	
-							<line 
-								x1={this.scaleFreq(d[role].freq.female)} y1={0} 
-								x2={this.scaleFreq(d[role].freq.male)} y2={0} 
-								style= {{ stroke: "#333" }}/>	
-							<text transform={`translate(${xOffset}, 0)`}>
-								{d.sector}
-							</text>
+								<line 
+									className="axis"
+									x1={ xMin } y1={0} 
+									x2={ xMax } y2={0}/>	
+								<line 
+									className="gap"
+									x1={ xMiddle } y1={0} 
+									x2={ xMiddle } y2={0} />	
+								<circle 
+									className="male"
+									cx={ xMiddle } cy={0} r={0} />	
+								<circle 
+									className="female"
+									cx={ xMiddle } cy={0} r={0}/>	
+								<text 
+									className="label"
+									dx={-7} dy={2}>
+									{d.sector}
+								</text>
 							</g>
 						);
 					})
 				}
+				</g>
 			</svg>
 		);
 	}

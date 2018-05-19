deda1ea63c37e804128521f3b958b0af06511a14
maryzam <zamo.code@gmail.com>
2018-05-19 13:55:44 +0300

    Update footer & header

--- src/components/App.js
+++ src/components/App.js
@@ -6,25 +6,12 @@ import OverallBySectorStats from "./OverallBySector/Section";
 import OverallByRaceStats from "./OverallByRace/Visualization";
 import TopCompanies from "./TopCompanies/Visualization";
 
-const Header = (props) => ( 
-	<header>TODO: Header</header>
-);
-
-const Footer = (props) => ( 
-	<footer>TODO: Footer</footer>
-);
-
 const App = (props) => (
-		<div>
-			<Header />
-			<div className="main"
-				 style={ { border: "1px solid black", padding: "20px" }} >
-				 <OverallByGenderStats {...props} />
-				 <OverallBySectorStats {...props} />
-				 <OverallByRaceStats {...props} />
-				 <TopCompanies {...props} />
-			</div>
-			<Footer />
+		<div className="main">
+			 <OverallByGenderStats {...props} />
+			 <OverallBySectorStats {...props} />
+			 <OverallByRaceStats {...props} />
+			 <TopCompanies {...props} />
 		</div>
 	);
 
